3a0872cdffbd416e3fd74db59f5143521f52b79a
Olivier Barais <barais@irisa.fr>
2015-01-28 16:20:52 +0100

    Integrate clickable image

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -6,4 +6,22 @@ angular.module('jhipsterApp')
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
+        $scope.$on('$viewContentLoaded', function(){
+          function blackNote() {
+            return $(document.createElement('span')).
+              addClass('black circle note')
+          }
+          console.log($('#nutmeg').get());
+          $('#nutmeg').annotatableImage(blackNote);
+
+          $('#serialize').click(function(event){
+            event.preventDefault();
+            console.log("ok");
+            $.each($('#nutmeg span.note').seralizeAnnotations(), function(){
+              console.log('x: ' + this.x + ' y: ' + this.y + ' response_time: ' + this.response_time + 'ms');
+            });
+
+          });
+        });
+
     });
