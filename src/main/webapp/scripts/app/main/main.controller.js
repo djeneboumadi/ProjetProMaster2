d2083f38beb6b65cf77bf28afd43dd90ffd9b8d4
Slelou <lelou.sylvain@gmail.com>
2015-02-04 10:25:04 +0100

    Annotorious is now working

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -7,22 +7,14 @@ angular.module('ludecolApp')
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
         
-        $scope.$on('$viewContentLoaded', function(){
-          function blackNote() {
-            return $(document.createElement('span')).
-              addClass('black circle note')
-          }
-          console.log($('#nutmeg').get());
-          $('#nutmeg').annotatableImage(blackNote);
-
-          $('#serialize').click(function(event){
-            event.preventDefault();
-            console.log("ok");
-            $.each($('#nutmeg span.note').seralizeAnnotations(), function(){
-              console.log('x: ' + this.x + ' y: ' + this.y + ' response_time: ' + this.response_time + 'ms');
-            });
-
-          });
-        });
-
     });
+    
+angular.module('ludecolApp')
+	.directive('annotorious', function() {
+	  return {
+	    restrict : 'A',
+	    link : function(scope, element) {
+	      anno.makeAnnotatable(element[0]);
+    	}
+  	};
+});
