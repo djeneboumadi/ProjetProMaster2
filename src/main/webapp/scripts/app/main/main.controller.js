3134713a99ddf719be99d02cb1c76a78005d67b9
Olivier Barais <barais@irisa.fr>
2015-02-17 16:35:02 +0100

    anatorious bug fix

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -5,32 +5,37 @@ angular.module('ludecolApp')
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
-        });
-        
+        });        
+        $scope.getAnnotations = function(el) {
+        	  var all = anno.getAnnotations();
+        	  var message = "";
+        	  all.forEach(function(entry) {
+        	  	var etu = entry.shapes[0].geometry;
+        	  	var x = etu.x + etu.width/2;
+        	  	var y = etu.y + etu.height/2;
+        	  		  	
+        	    message += "text : " + entry.text + "	|	posx :" + x + "	|	posy :" +  y + " \r ";
+        	  });
+        	  $scope.message = message;
+        	  
+          	 };
     });
-    
+
+
+
 angular.module('ludecolApp')
 	.directive('annotorious', function() {
 	  return {
 	    restrict : 'A',
 	    link : function(scope, element) {
-	      anno.makeAnnotatable(element[0]);
+	    	
+	    	element[0].addEventListener('load', function() {
+	    		anno.makeAnnotatable(element[0]);
+            });
+	    
+	    	
     	}
   	};
 });
 
   
-function getAnnotations(el) {
-	var $scope = angular.element(el).scope();
-      var all = anno.getAnnotations();
-	  var message = "";
-	  all.forEach(function(entry) {
-	  	var etu = entry.shapes[0].geometry;
-	  	var x = etu.x + etu.width/2;
-	  	var y = etu.y + etu.height/2;
-	  		  	
-	    message += "text : " + entry.text + "	|	posx :" + x + "	|	posy :" +  y + " \r ";
-	  });
-	  $scope.message = message;
-	  $scope.$apply();
-  	 }
