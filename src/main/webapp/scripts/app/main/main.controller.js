8f981f76a5a7aa625a8dc1611edfffa0b2762896
Olivier Barais <barais@irisa.fr>
2015-02-18 09:22:17 +0100

    new

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -28,6 +28,54 @@ angular.module('ludecolApp')
            	    }]
            	  }
            	});
+        	 annotorious.plugin.HelloWorldPlugin = function(opt_config_options) { }
+
+        	 annotorious.plugin.HelloWorldPlugin.prototype.initPlugin = function(anno) {
+        	   // Add initialization code here, if needed (or just skip this method if not)
+        	 }
+        	 $scope.species1 = true;
+        	 $scope.species2 = false;
+        	 annotorious.plugin.HelloWorldPlugin.prototype.onInitAnnotator = function(annotator) {
+        	   // A Field can be an HTML string or a function(annotation) that returns a string
+        	   annotator.editor.addField(function(annotation) {
+        		   var container = document.createElement('div');  
+        		   
+        		   var node = document.createElement('div');  
+        		   var newlabel = document.createElement("Label");
+        		   newlabel.innerHTML = "Species 1";
+        			var cb = document.createElement('input');
+             	    cb.type = 'checkbox';
+             	    cb.name = "species1";
+             	    cb.checked = $scope.species1;
+             	   cb.addEventListener('click', function() {
+             	      $scope.species1= cb.checked;
+             	   });
+             	  node.appendChild(cb);
+             	  node.appendChild(newlabel);
+            	   
+             	 var node1 = document.createElement('div');  
+      		   var newlabel1 = document.createElement("Label");
+      		   newlabel1.innerHTML = "Species 2";
+      			var cb1 = document.createElement('input');
+           	    cb1.type = 'checkbox';
+           	    cb1.name = "species2";
+           	    cb1.checked = $scope.species2;
+           	   cb1.addEventListener('click', function() {
+           	      $scope.species2= cb1.checked;
+           	   });
+           	  node1.appendChild(cb1);
+           	  node1.appendChild(newlabel1);
+           	  
+           	  container.appendChild(node);
+           	  container.appendChild(node1);
+          	  
+             	    return container;
+        	   });
+        	 }
+        
+        	 
+        	 // Add the plugin like so
+        	 anno.addPlugin('HelloWorldPlugin', {});
         	 $scope.myannotations = [];
 	    		anno.addHandler("onAnnotationCreated", function (an) {
 	    			$scope.myannotations.push(an);
@@ -48,7 +96,7 @@ angular.module('ludecolApp')
         	  var all = $scope.myannotations;
         	  var message = "";
         	  var annots = []
-        	  
+        	  console.log($scope.foo);
         	  all.forEach(function(entry) {
         		var etu = entry.shapes[0].geometry;
         	  	var x = etu.x + etu.width/2;
